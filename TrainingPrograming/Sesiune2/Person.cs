3e25c78a99cd7094e74e10b7da1d4694cd4a9dc5
Alexandrina <sandadragos@yahoo.co.uk>
2025-01-03 14:13:37 +0200

    Tema Clase si Metode

---
+++ TrainingPrograming/Sesiune2/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TrainingPrograming.Sesiune2
+{
+    internal class Person
+    {
+        string nume;
+        string prenume;
+        string gen;
+        int age;
+        int x;
+
+
+        [Test]
+        public void MetodaCalcul()
+        {
+            age = 47;
+            x = 20;
+            int calculateAge = age + x;
+            Console.WriteLine("You will be " + calculateAge + " years old in 15 years!");
+        }
+
+        [Test]
+        public void MetodaDisplay()
+        {
+            nume = "Marcu";
+            prenume = "Alexandrina";
+            gen = "Female";
+            age = 47;
+            int calculateAge = age + 15;
+            Console.WriteLine("The person's details are as follows:");
+            Console.WriteLine("- First Name: " + prenume);
+            Console.WriteLine("- Last Name: " + nume);
+            Console.WriteLine("- Gender: " + gen);
+            Console.WriteLine("You will be " + calculateAge + " years old in 15 years!");
+        }
+    }
+ }
+
+
+
+
+    
+
